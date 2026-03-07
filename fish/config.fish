79c5e634e8ba20100cc6285cb112920c409dcaf6
itsRaCl <rajclerk.2004@gmail.com>
2026-03-07 16:35:40 +0530

    restyle

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,9 @@ set fish_greeting
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/racl/.gcloud/google-cloud-sdk/path.fish.inc' ]; . '/home/racl/.gcloud/google-cloud-sdk/path.fish.inc'; end
 
+# Force Firefox to use native Wayland
+set -gx MOZ_ENABLE_WAYLAND 1
+
 starship init fish | source
 zoxide init fish --cmd cd | source
 
