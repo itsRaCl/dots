7f8c95e8516e0f2d43cc4b0da249c6fbe5d9feaf
itsRaCl <rajclerk.2004@gmail.com>
2026-03-14 00:00:45 +0530

    move to rofi from wofi

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting
+set -U fish_greeting ""
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/racl/.gcloud/google-cloud-sdk/path.fish.inc' ]; . '/home/racl/.gcloud/google-cloud-sdk/path.fish.inc'; end
